61f68fc0b8b59cc2488e3caca84b1b1e59d3f732
Toko_dev <layesall05@gmail.com>
2020-03-31 21:58:21 +0000

    Mise à jour de l'exercice 03

--- exo3/exercice03.php
+++ exo3/exercice03.php
@@ -1,8 +1,8 @@
 <?php
-function counte($objet){ // fonction pour calculer la taille d'un objet donne
-        $taille=0; // taille
+function taille($objet){
+        $taille=0; 
         $index=0;
-        while ( isset($objet[$index]) ) { // verification de l'element a un position donne
+        while ( isset($objet[$index]) ) { 
             $taille++;
             $index++;
         }
@@ -10,22 +10,22 @@ function counte($objet){ // fonction pour calculer la taille d'un objet donne
      }
      function nombreMotsContenantM($tabd){
         $somme =0;
-       for ($i=1; $i < counte($tabd) ; $i++) { 
+       for ($i=0; $i <taille($tabd) ; $i++) { 
           if(preg_match("#[mM]#",$tabd[$i])){
               $somme ++;
           }
        }
        return $somme;
      }
-     function isNombreValide($donne){ // fonction pour valider un nombre
+     function est_numeric($donne){
          $tabChifre=['0','1','2','3','4','5','6','7','8','9',' '];
          $bool=1;
          $index=0;
-         while ( $index < counte($donne) and $bool == 1 ){
+         while ( $index < taille($donne) and $bool == 1 ){
 
             $i=0;
             $bool2=0;
-            while ($i < counte($tabChifre) and $bool2 == 0){
+            while ($i < taille($tabChifre) and $bool2 == 0){
                 if ($tabChifre[$i]== $donne[$index]) {
                     $bool2 =1;
                 }
@@ -38,14 +38,14 @@ function counte($objet){ // fonction pour calculer la taille d'un objet donne
          }
          return $bool;
      }
-     function isMotsValide($donne){ // fonction pour valider un nombre
+     function est_chaine($donne){
          $tabChifre=['0','1','2','3','4','5','6','7','8','9',' ',',','-',';','?','/','\\','.',''];
          $bool=1;
          $index=0;
-         while ( $index < counte($donne) and $bool == 1 ){
+         while ( $index < taille($donne) and $bool == 1 ){
             $i=0;
             $bool2=0;
-            while ($i < counte($tabChifre) and $bool2 == 0){
+            while ($i < taille($tabChifre) and $bool2 == 0){
                 if ($tabChifre[$i]== $donne[$index]) {
                     $bool2 =1;
                 }
@@ -59,24 +59,23 @@ function counte($objet){ // fonction pour calculer la taille d'un objet donne
          return $bool;
      }
 
-     function reaffissageFormulair($val){
+     function formulaire($val){
       $form='';
       for ($i=1; $i <=$val ; $i++) { 
-          if ( isMotsValide($_POST['mots'.$i])==1) {
+          if ( est_chaine($_POST['mots'.$i])==1) {
             $form=$form.'<input type="text" class="in-word" name="mots'.$i.'" value="'.$_POST['mots'.$i].'">';
           }else{
             $form=$form.'<br><label style="color:red;">Ce mot est invalide</label>';
            $form=$form.'<input class="in-word" type="text" name="mots'.$i.'" value="'.$_POST['mots'.$i].'"><br>';
           }
       }
+      $form=$form.'<input class="btn btn-validate" value="Résultat" id="btn" name="resultat" type="submit"> ';
       return $form;
      }
-    $val;
-      $msg="";
-      $msg2="";
+      $val;
+      $succes="";
+      $erreur="";
       $form="";
-      $chtmp="";
-      $tab =array();
       $tabMotsValide =array();
       $tabMostInValide =array();
       $j=0;
@@ -86,33 +85,42 @@ function counte($objet){ // fonction pour calculer la taille d'un objet donne
         for ($i=1; $i<=$val ; $i++) { 
           $form=$form.'<input type="text" placeholder="Mot n°'.$i.'" class="in-word" name="mots'.$i.'">';
         }
-         if (isNombreValide($val)==1 && $val>0) { // recuperation des mots 
+        $form=$form.'<input class="btn btn-validate" value="Résultat" id="btn" name="resultat" type="submit"> ';
+         if (est_numeric($val)==1 && $val>0) { 
         for ($i=1; $i <=$val; $i++) { 
-           if (isset($_POST['mots'.$i])) {
-              if (isMotsValide($_POST['mots'.$i])==1 && counte($_POST['mots'.$i]) <=20) {
-                  $tabMotsValide[] = $_POST['mots'.$i]; // si mots valide
+           if (isset($_POST['resultat'])) {
+              if(!empty($_POST['mots'.$i])){
+                if (est_chaine($_POST['mots'.$i])==1 && taille($_POST['mots'.$i]) <=20) {
+                  $tabMotsValide[] = $_POST['mots'.$i]; 
               }else{
-                  $tabMostInValide[] = $_POST['mots'.$i]; //si mots invalide
+                  $tabMostInValide[] = $_POST['mots'.$i];
+                  $erreur="Le mot n ° ".$i." doit être une chaine de caractère et doit être supérieur à 20";
               }
-           }
+              if (taille($tabMostInValide)>0) { 
+              $form = formulaire($val);
+              $succes="Vueillez vérifier les mots que vous avez saisi";
+              }else{
+                $form = formulaire($val);
+              $succes='Les mots saisis sont : ';
+              foreach ($tabMotsValide as $key => $value) {
+                 $succes=$succes."$value | "; 
+              }
+              $nbrM = nombreMotsContenantM($tabMotsValide);
+              $succes=$succes."<div>Et les mots contenant la lettre « M/m » sont au nombre de <span class='nbreMotAvecM'>$nbrM</span></div>";
+              }
+            }else{
+              $form = formulaire($val);
+              $erreur="Tous les champs sont obligatoires";
+            }
         }
-        if (counte($tabMostInValide)>0) { // si il exixte des mots invalide
-         $form = reaffissageFormulair($val); //fonction qui reaffiche le formulaire en gardant les mots valide
-        }else{
-        $msg='Les mots saisis sont : ';
-        foreach ($tabMotsValide as $key => $value) { // recuperrations des mots valide dans le tableau
-
-           $msg=$msg."| $value | "; // concatenation des mots dans la variable $msg
         }
-        $nbrM = nombreMotsContenantM($tabMotsValide);
-        $msg=$msg."<div>Et les mots contenant la lettre « M/m » sont au nombre de <span class='nbreMotAvecM'>$nbrM</span></div>";
-        }
+        
       }else{
-         $msg2="Seuls les nombres sont autorisés";
+         $erreur="Seuls les nombres sont autorisés";
       }
-        // -----------------------------------------------------------------------------------
      }else{
-        $val=0;
+      $val='';
+        $erreur="Ce champ est obligatoire";
      }
         
 }
@@ -136,24 +144,24 @@ function counte($objet){ // fonction pour calculer la taille d'un objet donne
             <h3>Exercice 03 Serie_Tabaleu_Fonctions</h3>
                  <label for="nbrMots">Combien de mots voulez vous saisir ? </label>
                  <input type="text" value="<?php if(isset($_POST['val'])){echo $val ;} ?>" class="in-word" name="val" id="nbrMots">
+                 <input class="btn btn-validate" id="btn" name="valider" type="submit"> 
                  <?php if (isset($form)) {
                     echo $form;
-                 } ?>
-                 <input class="btn btn-validate" id="btn" name="valider" type="submit">            
+                 } ?>           
         </form>
   </div>
   <div class="result">
     <h2 class="titre">Résultat de l'algorithme</h2>
      <div>
             <?php
-            if (isset($msg)) {
+            if (isset($succes)) {
               ?>
               <h5>
-                <?php echo"$msg";?>     
+                <?php echo"$succes";?>     
             </h5>
             <h4>
-                <?php // Affissage du contenue des variable $msg et $msg2
-                    echo"$msg2"; 
+                <?php
+                    echo"$erreur"; 
                 ?>
             </h4>
             <?php

a53eaf2b9f0ad1f2c31286dbcb4b328520042519
Toko_dev <layesall05@gmail.com>
2020-05-27 00:07:13 +0000

    Initialisation de la v2 du quizz

---
+++ quizzSaDb/index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Index</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css" integrity="sha384-Bfad6CLCknfcloXFOyFnlgtENryhrpZCe29RTifKEixXQZ38WheV+i/6YWSzkz3V" crossorigin="anonymous">
+  </head>
+  <body>
+      <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-10 col-sm-12 offset-lg-3 offset-md-2 mt-5">
+                <div class="row">
+                    <div class="col-lg-10 col-md-10 col-sm-10 offset-sm-1 login-header">
+                        <img src="images/logo.png" alt="logo SA" class="logo"><br>
+                        <h3 class="login-header-title">Connexion</h3>
+                    </div>
+                    <div class="col-lg-10 col-md-10 col-sm-10 offset-sm-1 login-content pt-4">
+                        <form action="">
+                            <div class="form-group form-section">
+                            <input type="text" name="login" placeholder="Tapez votre login" class="form-control input-login">
+                            </div>
+                            <div class="form-group form-section">
+                            <input type="password" name="password" placeholder="Tapez votre login" class="form-control input-login">
+                            </div>
+                            <div class="form-group form-section">
+                            <input type="submit" name="valider" value="Se connecter" class="btn btn-success btn-login">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>

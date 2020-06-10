55b4b6b948a3d146681560b17c693c12c4d608f8
Toko_dev <layesall05@gmail.com>
2020-06-10 00:37:35 +0000

    US6: En tant que super administrateur, je peux gérer les utilisateurs

--- quizzdb/functions/db.php
+++ quizzdb/functions/db.php
@@ -1,12 +1,12 @@
 <?php
-$dbhost = 'mysql-tokosel-abdoulaye-sall.alwaysdata.net';
+/*$dbhost = 'mysql-tokosel-abdoulaye-sall.alwaysdata.net';
 $dbname = 'tokosel-abdoulaye-sall_saquizz';
 $dbuser = '208377_tokosel';
-$dbpswd = 'tokosel01++';
-/*$dbhost = 'localhost';
+$dbpswd = 'tokosel01++';*/
+$dbhost = 'localhost';
 $dbname = 'quizzdb';
 $dbuser = 'root';
-$dbpswd = '';*/
+$dbpswd = '';
 
 try{
     $db = new PDO('mysql:host='.$dbhost.';dbname='.$dbname,$dbuser,$dbpswd, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8', PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
